65b29acb8cc121cec760c9021741cf2db2326689
vladyslav.nasadiuk <vladyslav.nasadiuk@sigma.software>
2019-06-20 18:14:38 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {DragScrollModule} from 'cdk-drag-scroll/src/lib/drag-scroll.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    ScrollingModule,
+    DragScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
